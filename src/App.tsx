a61f24e33a0358c5d7dfe7423427860781b08aeb
Owen Zheng <zcc2280411284@gmail.com>
2024-06-05 10:01:53 -0700

    Added a filter button component for year filtering.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import CourseList from "./components/CourseList";
+import { Grid } from "@mui/material";
+import RadioGroupYearFilter from "./components/RadioGroupYearFilter";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <CourseList></CourseList>
+      <Grid container spacing={2}>
+        <Grid item xs={4}  md={3.5} lg={1}>
+          <div className="flexMiddle"><RadioGroupYearFilter></RadioGroupYearFilter></div>
+        </Grid>
+        <Grid item xs={8}  md={8.5} lg={11}>
+          <CourseList></CourseList>
+        </Grid>
+      </Grid>
     </>
   );
 }
