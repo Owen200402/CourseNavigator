9825534935a48b0d92766d64b15392331648817c
Owen Zheng <zcc2280411284@gmail.com>
2024-06-07 15:59:13 -0700

    Discard Chakra's idea as it fails to combine with mui.

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import { ThemeProvider } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>
 );
