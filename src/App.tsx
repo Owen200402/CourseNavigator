cf4c4b8de5b5b20ad81259fb1e753b9bddab5a02
Owen Zheng <zcc2280411284@gmail.com>
2024-06-10 10:09:07 -0700

    Configured scss.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Box, Grid, Typography } from "@mui/material";
 import RadioGroupYearFilter from "./components/RadioGroupYearFilter";
 import CourseFilter from "./components/CourseFilter";
 import styled from "@emotion/styled";
-import './custom.scss';
+import "./App.scss"
 
 const Title = styled(Typography)`
   white-space: nowrap;
