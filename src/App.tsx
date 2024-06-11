9d63878c1d9e62b2dc97327d928fb7c9cd6ed665
Owen Zheng <zcc2280411284@gmail.com>
2024-06-11 11:21:26 -0700

    Path error.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Box, Grid, Typography } from "@mui/material";
 import RadioGroupYearFilter from "./components/RadioGroupYearFilter";
 import CourseFilter from "./components/CourseFilter";
 import styled from "@emotion/styled";
-import CourseTree from "./components/CourseTree";
 
 const Title = styled(Typography)`
   white-space: nowrap;
